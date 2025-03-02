adfb79df27dfb634ef9d3aeca4162c22903cfcaf
Patty Karigi Kariuki <pattykarigi96@gmail.com>
2025-03-02 17:29:46 +0300

    Initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate",4,"M","white","aggressive",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy",4,"M","brown","cool",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha",4,"F","black","aggressive",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",4,"chocolate","orange","aggressive",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa",4,"F","grey","cool",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch",4,"M","white","aggressive",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy",4,"F","brown","cool",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,4,"M","black","aggressive",1);
